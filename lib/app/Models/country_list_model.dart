ffb6c639c4e65a3a3855cf97a67d090b23922ab2
da2ja3 <da2ja3@gmail.com>
2022-11-13 23:26:57 +0100

    second commit

--- lib/app/Models/country_list_model.dart
+++ lib/app/Models/country_list_model.dart
@@ -1,26 +1,58 @@
-class Countrymodel {
-  Countrymodel({
-    this.userId,
-    this.id,
-    this.title,
-    this.body,
+import 'package:equatable/equatable.dart';
+
+class CountryModel extends Equatable {
+  const CountryModel({
+    required this.nameCommon,
+    required this.nameOfficial,
+    required this.capital,
+    required this.region,
+    required this.subregion,
+    required this.area,
+    required this.population,
+    required this.continents,
+    required this.flagPng,
+    this.unGrouped = 'All Countries',
   });
 
-  int? userId;
-  int? id;
-  String? title;
-  String? body;
+  final String nameCommon;
+  final String nameOfficial;
+  final String capital;
+  final String region;
+  final String subregion;
+  final double area;
+  final int population;
+  final String continents;
+  final String flagPng;
+  final String unGrouped;
 
-  Countrymodel copyWith({
-    int? userId,
-    int? id,
-    String? title,
-    String? body,
-  }) =>
-      Countrymodel(
-        userId: userId ?? this.userId,
-        id: id ?? this.id,
-        title: title ?? this.title,
-        body: body ?? this.body,
-      );
+  @override
+  List<Object?> get props => [
+    nameCommon,
+    nameOfficial,
+    capital,
+    region,
+    subregion,
+    area,
+    population,
+    continents,
+    flagPng,
+  ];
+
+  factory CountryModel.fromJson(Map<String, dynamic> json) {
+    return CountryModel(
+      nameCommon: json["name"]["common"],
+      nameOfficial: json["name"]["official"],
+      capital: List<String>.from(
+          json["capital"]?.map((cap) => cap) ?? ['No Capital'])
+          .toString()
+          .replaceAll(']', '')
+          .replaceAll('[', ''),
+      region: json["region"],
+      subregion: json["subregion"] ?? 'No Subregion',
+      area: json["area"],
+      population: json["population"],
+      continents: json["continents"][0],
+      flagPng: json["flags"]["png"],
+    );
+  }
 }
